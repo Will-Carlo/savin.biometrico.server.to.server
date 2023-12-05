aaba9c534eb46304a8fef4922ec2fd2c430708e8
Will-Carlo <wil_fer_25@outlook.es>
2023-12-05 09:25:46 -0400

    dando de baja botones /options para adm y reportes

--- resources/views/options.blade.php
+++ resources/views/options.blade.php
@@ -24,8 +24,8 @@
 
     <div class="">
         <a href="{{route('delay')}}" class="btn btn-sm btn-primary">Ver Atrasos</a>
-        <a href="{{route('register')}}" class="btn btn-sm btn-primary">Registrar nuevo personal</a>
-        <a href="{{route('adm')}}" class="btn btn-sm btn-primary">Reportes</a>
+        <!-- <a href="{{route('register')}}" class="btn btn-sm btn-primary">Registrar nuevo personal</a> -->
+        <!-- <a href="{{route('adm')}}" class="btn btn-sm btn-primary">Reportes</a> -->
 
         <a href="{{route('verify')}}" class="btn btn-danger btn-sm float-rigth">Volver</a>
     </div>
